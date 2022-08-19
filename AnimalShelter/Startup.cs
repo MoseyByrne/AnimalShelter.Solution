c519b7e9d931caf0177e4cb68f593f8953bdcb13
MoseyByrne <mosey.b.byrne@gmail.com>
2022-08-19 11:48:18 -0700

    WIP Swagger

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
+using System.IO;
+using System.Reflection;
 
 namespace AnimalShelter
 {
@@ -49,7 +51,48 @@ namespace AnimalShelter
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                                
             });
+
+            // //Register the Swagger generator, defining 1 or more Swagger documents
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo
+            //     {
+            //         Version = "v1",
+            //         Title = "AnimalShelter API",
+            //         Description = "A simple example ASP.NET Core Web API",
+            //         TermsOfService = new Uri("https://example.com/terms"),
+            //         Contact = new OpenApiContact
+            //         {
+            //             Name = "Mo Byrne",
+            //             Email = string.Empty,
+            //             Url = new Uri("https://github.com/moseybyrne/AnimalShelter.Solution"),
+            //         },
+            //         License = new OpenApiLicense
+            //         {
+            //             Name = "Use under LICX",
+            //             Url = new Uri("https://example.com/license"),
+            //         }
+            //     });
+
+            //                //Enable middleware to serve generated Swagger as a Json endpoint
+            // app.UseSwagger(c =>
+            // {
+            //     c.SerializeAsV2 = true;
+            // });
+
+            // //Enable middle ware to serve swagger-ui (HTML, JS, CSS, etc.)
+            // //specify the Swagger JSON endpoint.
+            // app.UseSwaggerUI(c => 
+            // {
+            //     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My House Plant API V1");
+            //     c.RoutePrefix = string.Empty;
+            // });
+
+            // });
         }
     }
 }
+        
+    
